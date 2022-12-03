cbc8daaedcf9de0e8cd551691d0b052a1c54e7dc
Sushmasunkara <119720369+Sushmasunkara@users.noreply.github.com>
2022-12-03 11:25:52 +0530

    #include<stdio.h> int main() {     int sp,cp;     scanf("%d%d",&cp,&sp);     if(sp>cp)     {         printf("Profit");     }     else     {         printf("Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int sp,cp;
+    scanf("%d%d",&cp,&sp);
+    if(sp>cp)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+}
