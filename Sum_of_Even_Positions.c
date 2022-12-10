4629bed8f0883bd80d27cbdabc2d7b92ede2e76f
Sushmasunkara <119720369+Sushmasunkara@users.noreply.github.com>
2022-12-10 11:37:09 +0530

    #include<stdio.h> int main() {     int N,i;     scanf("%d",&N);     int a[N],sum=0;     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {         if(i%2==0)         {             sum=sum+a[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int N,i;
+    scanf("%d",&N);
+    int a[N],sum=0;
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        if(i%2==0)
+        {
+            sum=sum+a[i];
+        }
+    }
+    printf("%d",sum);
+}
