9a1da15c00b916d767bb8b3c3966430c91e31059
Sushmasunkara <119720369+Sushmasunkara@users.noreply.github.com>
2022-12-21 21:08:07 +0530

    #include<stdio.h> int main() {     int m,i;     scanf("%d",&m);     for(i=1;i<=m;i++)     {         int n,q,r,s=0;         scanf("%d",&n);         q=n;         while(q!=0)         {             r=q%10;             s=s*10+r;             q=q/10;         }         if(s==n)         {             printf("True ");         }         else         {             printf("False ");         }     }
    }

---
+++ Palindrome_Number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int m,i;
+    scanf("%d",&m);
+    for(i=1;i<=m;i++)
+    {
+        int n,q,r,s=0;
+        scanf("%d",&n);
+        q=n;
+        while(q!=0)
+        {
+            r=q%10;
+            s=s*10+r;
+            q=q/10;
+        }
+        if(s==n)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+    
+    
+}
