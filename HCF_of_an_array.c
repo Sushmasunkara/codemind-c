08154f0ec371e589939a7fa55a261cc5e23a8799
Sushmasunkara <119720369+Sushmasunkara@users.noreply.github.com>
2023-01-05 11:02:12 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,s,max=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]>max)         max=arr[i];     }     for(max;max>0;max--)     {         for(i=0;i<n;i++)         {             if(arr[i]%max!=0)             {                 s=0;                 break;             }             else             s=1;         }         if(s==1)         {             printf("%d",max);             break;         }     } }

---
+++ HCF_of_an_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,s,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    for(max;max>0;max--)
+    {
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%max!=0)
+            {
+                s=0;
+                break;
+            }
+            else
+            s=1;
+        }
+        if(s==1)
+        {
+            printf("%d",max);
+            break;
+        }
+    }
+}
